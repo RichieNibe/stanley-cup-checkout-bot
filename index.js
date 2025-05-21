74589c2af214de7cc83307e15f390a63a6c28c19
RichardNibe <RICHARDNIBE2@GMAIL.COM>
2025-05-20 23:51:43 -0400

    refactor: clean up code formatting and improve readability in checkout functions

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ puppeteer.use(StealthPlugin())
 async function loadPage(url) {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  return  page;
+  return page;
 }
 async function addToCart(page) {
   new Promise((resolve) => setTimeout(resolve, 2000));
@@ -19,56 +19,56 @@ async function addToCart(page) {
 
   await page.waitForSelector("button[name='checkout']");
   await page.evaluate(() => {
-     document.querySelector("button[name='checkout']").click();
+    document.querySelector("button[name='checkout']").click();
   })
-  
-   
+
+
 }
 
 async function checkOut(page) {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-    const emailInput = await page.waitForSelector("#email");
-    await emailInput.type("tuvcxmaxsuutadmkac@xfavaj.com"); 
-    const firstNameInput = await page.waitForSelector("#TextField0");
-    await firstNameInput.type("John");
-    const lastNameInput = await page.waitForSelector("#TextField1");
-    await lastNameInput.type("Doe");
-    const addressInput = await page.waitForSelector("#shipping-address1");
-    await addressInput.type("2401 Elliott Ave");
-    const cityInput = await page.waitForSelector("#TextField4");
-    await cityInput.type("Seattle");
-    const phoneInput = await page.waitForSelector("#TextField6");
-    await phoneInput.type("2345678901");
-    await page.select("select[name='zone']", "WA");
-    const zipCodeInput = await page.waitForSelector("#TextField5");
-    await zipCodeInput.type("98121");
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const emailInput = await page.waitForSelector("#email");
+  await emailInput.type("tuvcxmaxsuutadmkac@xfavaj.com");
+  const firstNameInput = await page.waitForSelector("#TextField0");
+  await firstNameInput.type("John");
+  const lastNameInput = await page.waitForSelector("#TextField1");
+  await lastNameInput.type("Doe");
+  const addressInput = await page.waitForSelector("#shipping-address1");
+  await addressInput.type("2401 Elliott Ave");
+  const cityInput = await page.waitForSelector("#TextField4");
+  await cityInput.type("Seattle");
+  const phoneInput = await page.waitForSelector("#TextField6");
+  await phoneInput.type("2345678901");
+  await page.select("select[name='zone']", "WA");
+  const zipCodeInput = await page.waitForSelector("#TextField5");
+  await zipCodeInput.type("98121");
 
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-    const continueToShipping = await page.waitForSelector("button[type='submit']");
-    await continueToShipping.click();
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const continueToShipping = await page.waitForSelector("button[type='submit']");
+  await continueToShipping.click();
 
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-    const continueToPayment = await page.waitForSelector("button[type='submit']");
-    await continueToPayment.click();
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const continueToPayment = await page.waitForSelector("button[type='submit']");
+  await continueToPayment.click();
 
-    const continueToPayment2 = await page.waitForSelector("button[type='submit']");
-    await continueToPayment2.click();
-    
-    await fillPaymentInfo(page);
+  const continueToPayment2 = await page.waitForSelector("button[type='submit']");
+  await continueToPayment2.click();
+
+  await fillPaymentInfo(page);
 }
 
-async function fillPaymentInfo(page) { 
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-    const cardNumberInput = await page.waitForSelector("#number");
-    await cardNumberInput.type(" 4807706658353551", { delay: 100 });
-    const cardExpiryInput = await page.waitForSelector("#expiry");
-    await cardExpiryInput.type("03/2027", { delay: 300 });
-    const cardCvcInput = await page.waitForSelector("#verification_value");
-    await cardCvcInput.type(" 687", { delay: 1000 });
+async function fillPaymentInfo(page) {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const cardNumberInput = await page.waitForSelector("#number");
+  await cardNumberInput.type(" 4807706658353551", { delay: 100 });
+  const cardExpiryInput = await page.waitForSelector("#expiry");
+  await cardExpiryInput.type("03/2027", { delay: 300 });
+  const cardCvcInput = await page.waitForSelector("#verification_value");
+  await cardCvcInput.type(" 687", { delay: 1000 });
 
-    const payButton = await page.waitForSelector("button[type='submit']");
-    await payButton.click();
-    
+  const payButton = await page.waitForSelector("button[type='submit']");
+  await payButton.click();
+
 }
 
 
