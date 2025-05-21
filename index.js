4c1f36e451ff0c9945f0e6fd674dd9f03134aa34
RichieNibe <Richardnibe2@gmail.com>
2025-05-20 23:46:21 -0400

    feat: enhance checkout process by adding shipping and payment steps

--- index.js
+++ index.js
@@ -15,44 +15,60 @@ async function addToCart(page) {
   const addToCartButton = await page.waitForSelector("#ProductSubmitButton-");
   await addToCartButton.click();
 
-  new Promise((resolve) => setTimeout(resolve, 10000));
+  await new Promise((resolve) => setTimeout(resolve, 2000));
 
   await page.waitForSelector("button[name='checkout']");
   await page.evaluate(() => {
      document.querySelector("button[name='checkout']").click();
-  })
-
-  new Promise((resolve) => setTimeout(resolve, 2000));
-
-  await page.waitForSelector("#cart-icon-bubble");
-  await page.evaluate(() => {
-     document.querySelector("#cart-icon-bubble").click();
   })
   
    
 }
 
 async function checkOut(page) {
-    const checkoutButton = await page.waitForSelector(".checkout-button");
-    await checkoutButton.click();
+    await new Promise((resolve) => setTimeout(resolve, 2000));
     const emailInput = await page.waitForSelector("#email");
-    await emailInput.type("");
-    const continueButton = await page.waitForSelector(".continue-button");    
+    await emailInput.type("tuvcxmaxsuutadmkac@xfavaj.com"); 
+    const firstNameInput = await page.waitForSelector("#TextField0");
+    await firstNameInput.type("John");
+    const lastNameInput = await page.waitForSelector("#TextField1");
+    await lastNameInput.type("Doe");
+    const addressInput = await page.waitForSelector("#shipping-address1");
+    await addressInput.type("2401 Elliott Ave");
+    const cityInput = await page.waitForSelector("#TextField4");
+    await cityInput.type("Seattle");
+    const phoneInput = await page.waitForSelector("#TextField6");
+    await phoneInput.type("2345678901");
+    await page.select("select[name='zone']", "WA");
+    const zipCodeInput = await page.waitForSelector("#TextField5");
+    await zipCodeInput.type("98121");
+
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    const continueToShipping = await page.waitForSelector("button[type='submit']");
+    await continueToShipping.click();
+
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    const continueToPayment = await page.waitForSelector("button[type='submit']");
+    await continueToPayment.click();
+
+    const continueToPayment2 = await page.waitForSelector("button[type='submit']");
+    await continueToPayment2.click();
+    
+    await fillPaymentInfo(page);
 }
 
-async function fillShippingInfo(page) { 
-    const firstNameInput = await page.waitForSelector("#first-name");
-    await firstNameInput.type("John");
-    const lastNameInput = await page.waitForSelector("#last-name");
-    await lastNameInput.type("Doe");
-    const addressInput = await page.waitForSelector("#address");
-    await addressInput.type("123 Main St");
-    const cityInput = await page.waitForSelector("#city");
-    await cityInput.type("Anytown");
-    const stateInput = await page.waitForSelector("#state");
-    await stateInput.type("CA");
-    const zipCodeInput = await page.waitForSelector("#zip-code");
-    await zipCodeInput.type("12345");
+async function fillPaymentInfo(page) { 
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    const cardNumberInput = await page.waitForSelector("#number");
+    await cardNumberInput.type(" 4807706658353551", { delay: 100 });
+    const cardExpiryInput = await page.waitForSelector("#expiry");
+    await cardExpiryInput.type("03/2027", { delay: 300 });
+    const cardCvcInput = await page.waitForSelector("#verification_value");
+    await cardCvcInput.type(" 687", { delay: 1000 });
+
+    const payButton = await page.waitForSelector("button[type='submit']");
+    await payButton.click();
+    
 }
 
 
@@ -60,7 +76,7 @@ async function run() {
   const page = await loadPage();
   await page.goto("https://www.stanley1913.com/products/the-club-vintage-quencher-h2-0-flowstate%E2%84%A2-tumbler-30-oz?variant=53972848050536");
   await addToCart(page);
- 
+  await checkOut(page);
 
 }
 
