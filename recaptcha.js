de74395f1993b745300bd61618338ac891f0a3e6
RichieNibe <Richardnibe2@gmail.com>
2025-05-22 17:38:44 -0400

    feat: add recaptcha handling with anti-captcha integration

---
+++ recaptcha.js
@@ -0,0 +1,54 @@
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+const ac = require("@antiadmin/anticaptchaofficial");
+ac.setAPIKey('6629c482f7f1cd0f308664b4ec65bfcb');
+puppeteer.use(StealthPlugin())
+
+async function loadPage(url) {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  return page;
+}
+
+async function solveRecaptcha(url, page) {
+    ac.setSoftId(0);
+
+    ac.solveRecaptchaV2EnterpriseProxyless(
+        url,
+        '6Le-wvkSAAAAAPBMRTvw0Q4Muexq9bi0DJwx_mJ-',
+        {
+            "s" : "SOME_TOKEN",
+            "any_custom_parameter" : "string_number_or_boolean"
+        })
+        .then(gresponse => {
+            console.log('g-response: '+gresponse);
+            page.evaluate((gresponse) => {
+                document.getElementById('g-recaptcha-response').innerHTML = gresponse;
+                submit(page);
+            }, gresponse);
+            
+        })
+        .catch(error => console.log('test received error '+error));
+    
+    
+}
+
+async function submit(page) {
+  await page.waitForSelector("#recaptcha-demo-submit");
+  await page.evaluate(() => {
+    document.querySelector("#recaptcha-demo-submit").click();
+  })
+
+
+}
+
+async function run() {
+  const page = await loadPage();
+  await page.goto("https://google.com/recaptcha/api2/demo?invisible=false");
+  await solveRecaptcha("https://google.com/recaptcha/api2/demo?invisible=false", page);
+  
+
+
+}
+
+run();
